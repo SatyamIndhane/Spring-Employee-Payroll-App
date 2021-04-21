7e93c22ec1df72419ba419a37d6e0f83abce3635
SatyamIndhane <satyam.indhane@gmail.com>
2021-04-21 09:36:25 +0530

    Spring-Employee-Payroll-App

---
+++ src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
@@ -0,0 +1,36 @@
+package com.bridgelabz.employeepayrollapp.dto;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PastOrPresent;
+import javax.validation.constraints.Pattern;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import lombok.ToString;
+
+public @ToString class EmployeePayrollDTO {
+	@Pattern(regexp="^[A-Z]{1}[a-zA-Z]{2,}$",message="Employee Name Invalid")
+	public String name;
+	@Min(value=500,message="Employee Wage should be more than 500")
+	public long salary;
+	@Pattern(regexp="male|female",message="Gender needs to be male or female")
+	public String gender;
+	
+	@JsonFormat(pattern="dd MMM yyyy")
+	@NotNull(message="startDate should not be empty")
+	@PastOrPresent(message="startDate should be past or todays date")
+	public LocalDate startDate;
+	
+	@NotBlank(message="note should not be blank")
+	public String note;
+	@NotBlank(message="profilePic should not be blank")
+	public String profilePic;
+	
+	@NotNull(message = "department should not be empty")
+	public List<String> department;
+}
---
+++ src/main/java/com/bridgelabz/employeepayrollapp/model/EmployeePayrollData.java
@@ -0,0 +1,60 @@
+package com.bridgelabz.employeepayrollapp.model;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import javax.persistence.CollectionTable;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Table;
+
+import com.bridgelabz.employeepayrollapp.dto.EmployeePayrollDTO;
+
+import lombok.Data;
+
+@Entity
+@Table(name = "employee_payroll")
+public @Data class EmployeePayrollData {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "employee_id")
+	private int empId;
+	private String name;
+	private long salary;
+
+	public String gender;
+
+	@Column(name = "start_date")
+	public LocalDate startDate;
+	public String note;
+	@Column(name = "profile_pic")
+	public String profilePic;
+
+	@ElementCollection
+	@CollectionTable(name = "employee_department", joinColumns = @JoinColumn(name = "id"))
+	@Column(name = "department")
+	public List<String> departments;
+
+	public EmployeePayrollData() {
+	}
+
+	public EmployeePayrollData(EmployeePayrollDTO empPayrollDTO) {
+		this.updateEmployeePayrollData(empPayrollDTO);
+	}
+
+	public void updateEmployeePayrollData(EmployeePayrollDTO empPayrollDTO) {
+		this.name = empPayrollDTO.name;
+		this.salary = empPayrollDTO.salary;
+		this.gender = empPayrollDTO.gender;
+		this.startDate = empPayrollDTO.startDate;
+		this.note = empPayrollDTO.note;
+		this.profilePic = empPayrollDTO.profilePic;
+		this.departments = empPayrollDTO.department;
+	}
+}
